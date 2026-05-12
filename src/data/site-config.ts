e2dc5b7ec13aeb5edbe04e0358d7695f72e48cfd
Jiang Salim <jaingsalim@gmail.com>
2026-05-11 22:59:29 -0700

    Update site-config.ts

--- src/data/site-config.ts
+++ src/data/site-config.ts
@@ -18,7 +18,7 @@ export const siteConfig = {
     "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3989.XXXXX!2d33.XXXXX!3d0.XXXXX!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMMKwMDAnMDAuMCJOIDMzwrAwMCcwMC4wIkU!5e0!3m2!1sen!2sug!4vXXXXXXXXXX",
 
   social: {
-    linkedin: "https://linkedin.com/company/herman-software",
+    linkedin: "https://www.linkedin.com/in/jaing-salim-2641a6280?trk=contact-info",
     twitter: "https://twitter.com/hermansoftware",
     facebook: "https://facebook.com/hermansoftware",
   },
