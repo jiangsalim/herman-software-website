b8b395ea20a4d372f54c7e804ad55289d86f980e
Jiang Salim <jaingsalim@gmail.com>
2026-05-12 05:37:59 -0700

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -146,9 +146,30 @@ export default function RootLayout({
     {children}
   </PageTransition>
 </main>
-        <Footer />
-        <WhatsAppButton />
-        <BackToTop />
+    <Footer />
+    <WhatsAppButton />
+    <BackToTop />
+
+    {/* Tawk.to Live Chat */}
+    <script
+      type="text/javascript"
+      dangerouslySetInnerHTML={{
+        __html: `
+          var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+          (function(){
+            var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+            s1.async=true;
+            s1.src='https://embed.tawk.to/6a031e36b31dab1c398e1064/1joe2s2dm';
+            s1.charset='UTF-8';
+            s1.setAttribute('crossorigin','*');
+            s0.parentNode.insertBefore(s1,s0);
+          })();
+        `,
+      }}
+    />
+  </body>
+</html>
+        
       </body>
     </html>
   );
