98c27e6e07baef112b93358f9d620dcbc5e23ef4
jiangsalim <jaingsalim@gmail.com>
2026-05-19 18:53:56 +0300

    Add thumbnail to Sanity project queries

--- src/sanity/queries.ts
+++ src/sanity/queries.ts
@@ -1,7 +1,7 @@
 import { sanityClient } from "./client";
 
 export async function getBlogPosts() {
-  return sanityClient.fetch(`
+  return sanityClient.fetch(
     *[_type == "blogPost"] | order(publishedAt desc) {
       "slug": slug.current,
       title,
@@ -10,12 +10,12 @@ export async function getBlogPosts() {
       category,
       excerpt
     }
-  `);
+  );
 }
 
 export async function getBlogPost(slug: string) {
   return sanityClient.fetch(
-    `*[_type == "blogPost" && slug.current == $slug][0] {
+    *[_type == "blogPost" && slug.current ==  + "$slug" + ][0] {
       "slug": slug.current,
       title,
       author,
@@ -23,13 +23,13 @@ export async function getBlogPost(slug: string) {
       category,
       excerpt,
       body
-    }`,
+    },
     { slug }
   );
 }
 
 export async function getProjects() {
-  return sanityClient.fetch(`
+  return sanityClient.fetch(
     *[_type == "project"] | order(title asc) {
       "slug": slug.current,
       title,
@@ -37,22 +37,24 @@ export async function getProjects() {
       challenge,
       solution,
       result,
-      technologies
+      technologies,
+      "thumbnail": thumbnail.asset->url
     }
-  `);
+  );
 }
 
 export async function getProject(slug: string) {
   return sanityClient.fetch(
-    `*[_type == "project" && slug.current == $slug][0] {
+    *[_type == "project" && slug.current ==  + "$slug" + ][0] {
       "slug": slug.current,
       title,
       sector,
       challenge,
       solution,
       result,
-      technologies
-    }`,
+      technologies,
+      "thumbnail": thumbnail.asset->url
+    },
     { slug }
   );
 }
